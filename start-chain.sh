0defd3d8dff4e1c97b012f62e62bf25b1a99d5ef
Manuel Turetta <manuel.turetta94@gmail.com>
2022-09-21 18:57:34 +0200

    refactor: format start-chain.sh script

--- start-chain.sh
+++ start-chain.sh
@@ -29,40 +29,40 @@ prepare_chain() {
   user_genesis_file=$1
 
   ./desmos testnet --v 1 --keyring-backend=test \
-      --gentx-coin-denom="stake" --minimum-gas-prices="0.000006stake"
+    --gentx-coin-denom="stake" --minimum-gas-prices="0.000006stake"
 
-    # Generated genesis file path
-    node_genesis_file_path="mytestnet/node0/desmos/config/genesis.json"
+  # Generated genesis file path
+  node_genesis_file_path="mytestnet/node0/desmos/config/genesis.json"
 
   if test -f "$user_genesis_file"; then
     # Load genesis file
-      genesis_content=$(cat "$node_genesis_file_path")
-      # Append balances
-      balances=$(jq '.app_state.bank.balances' "$user_genesis_file")
-      genesis_content=$(echo "$genesis_content" | jq ".app_state.bank.balances += $balances")
-      # Append accounts
-      accounts=$(jq '.app_state.auth.accounts' "$user_genesis_file")
-      genesis_content=$(echo "$genesis_content" | jq ".app_state.auth.accounts += $accounts")
+    genesis_content=$(cat "$node_genesis_file_path")
+    # Append balances
+    balances=$(jq '.app_state.bank.balances' "$user_genesis_file")
+    genesis_content=$(echo "$genesis_content" | jq ".app_state.bank.balances += $balances")
+    # Append accounts
+    accounts=$(jq '.app_state.auth.accounts' "$user_genesis_file")
+    genesis_content=$(echo "$genesis_content" | jq ".app_state.auth.accounts += $accounts")
 
-      # Modules to copy into the genesis configurations
-      custom_modules=("profiles" "relationships" "subspaces" "posts" "reports" "reactions" "fees" "supply" "wasm")
-      for module in "${custom_modules[@]}"; do
-        module_content=$(jq ".app_state.$module" "$user_genesis_file")
-        genesis_content=$(echo "$genesis_content" | jq ".app_state.$module = $module_content")
-      done
+    # Modules to copy into the genesis configurations
+    custom_modules=("profiles" "relationships" "subspaces" "posts" "reports" "reactions" "fees" "supply" "wasm")
+    for module in "${custom_modules[@]}"; do
+      module_content=$(jq ".app_state.$module" "$user_genesis_file")
+      genesis_content=$(echo "$genesis_content" | jq ".app_state.$module = $module_content")
+    done
 
-      # Clear out genesis supply
-      genesis_content=$(echo "$genesis_content" | jq ".app_state.bank.supply = []")
+    # Clear out genesis supply
+    genesis_content=$(echo "$genesis_content" | jq ".app_state.bank.supply = []")
 
-      # Save genesis file
-      echo "$genesis_content" > "$node_genesis_file_path"
+    # Save genesis file
+    echo "$genesis_content" >"$node_genesis_file_path"
   fi
 }
 
 # Runs the chains in background saving the execution log inside the start-chain.log file.
 run_chain() {
   echo "test running from head"
-  ./desmos start --home="./mytestnet/node0/desmos" &> "./start-chain.log" &
+  ./desmos start --home="./mytestnet/node0/desmos" &>"./start-chain.log" &
 }
 
 # Runs the provided script.
@@ -89,10 +89,9 @@ wait_chain_start() {
   sleep 2
 
   block="$(./desmos q block | jq '.block')"
-  while [ "$block" == "null" ]
-  do
-     sleep 1
-     block="$(./desmos q block | jq '.block')"
+  while [ "$block" == "null" ]; do
+    sleep 1
+    block="$(./desmos q block | jq '.block')"
   done
 
   echo "Chain started!"
