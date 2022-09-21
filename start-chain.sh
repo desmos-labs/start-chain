e39261e68238ad1a73eac54d03352b8d9b5ac38b
Manuel Turetta <manuel.turetta94@gmail.com>
2022-09-21 19:06:38 +0200

    chore: removed debug echo

--- start-chain.sh
+++ start-chain.sh
@@ -61,7 +61,6 @@ prepare_chain() {
 
 # Runs the chains in background saving the execution log inside the start-chain.log file.
 run_chain() {
-  echo "test running from head"
   ./desmos start --home="./mytestnet/node0/desmos" &>"./start-chain.log" &
 }
 
